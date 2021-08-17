02fc8479e4ccc979dab58037e0aada6c4486b715
Ohjinn <hjin9445@gamil.com>
2021-08-17 21:04:58 +0900

    210816

--- src/section3/Main.java
+++ src/section3/Main.java
@@ -26,6 +26,18 @@ public class Main {
         return answer;
     }
 
+    public int solution6_1(int n, int k, int[] arr){
+        int answer = 0, cnt = 0, lt = 0;
+        for(int rt = 0; rt < n; rt++){
+            if(arr[rt] == 0) cnt++;
+            while(cnt > k){
+                if(arr[lt] == 0) cnt--;
+                lt++;
+            }
+            answer = Math.max(answer, rt-lt+1);
+        }
+        return answer;
+    }
 
     public static void main(String[] args) {
         Main T = new Main();
--- src/section3/c3_4.java
+++ src/section3/c3_4.java
@@ -19,6 +19,18 @@ public class c3_4 {
         return answer;
     }
 
+    private int solution4_1(int n, int m, int[] arr){
+        int answer = 0, sum = 0, lt = 0;
+        for(int rt = 0; rt < n; rt++){
+            sum += arr[rt];
+            if(sum == m) answer++;
+            while(sum >= m){
+                sum -= arr[lt++];
+                if(sum == m) answer++;
+            }
+        }
+        return answer;
+    }
 
     public static void main(String[] args) {
         c3_4 T = new c3_4();
