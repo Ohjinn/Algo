630be86388c1c532571febc9dfea40c04223cda7
Ohjinn <hjin9445@gmail.com>
2021-08-22 22:25:56 +0900

    210822

--- src/section5/Main.java
+++ src/section5/Main.java
@@ -5,12 +5,19 @@ import java.util.*;
 public class Main {
 
     private int solution(int n, int m){
-        int answer = 0;
+        int answer = 0, count = 0;
         int[] arr = new int[n + 1];
-        for(int i = 1; i <= n; i++) arr[i] = i;
-        for(int i = 1; i <= n; i++) {
-
+        for(int i = 1; i <= n; i++) arr[i] = 1;
+        for(int i = 1; i < n; i++) {
+            count = 0;
+            for(int j = i; count < m; j++){
+                if(arr[j] != 0) count++;
+                if(arr[j] == 0) j++;
+                if(count == m) arr[j] = 0;
+                j %= n;
+            }
         }
+        for(int i = 1; i < n; i++) if(arr[i] != 0) answer = i;
         return answer;
     }
 
